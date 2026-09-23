e6581377b10e6379ac6e191eb15d0554097d8540
PatrickWalter123 <a01053917426@gmail.com>
2026-09-23 11:04:32 +0900

    Release v1.1.8

--- src/ish/__init__.py
+++ src/ish/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import Any
 
-VERSION: tuple[int, int, int] = (1, 1, 7)
+VERSION: tuple[int, int, int] = (1, 1, 8)
 __version__: str = ".".join(map(str, VERSION))
 
 
